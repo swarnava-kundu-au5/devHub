95da445012dce222875afc5dedda161aa9ffb486
swarnava-kundu-au5 <17lsmi06@uohyd.ac.in>
2020-07-07 10:28:21 +0530

    production main

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 //connect database
 connectDB();
-app.get("/", (req, res) => res.send("API RUNNING"));
+
 //init middleware
 app.use(express.json({ extended: false }));
 
@@ -11,6 +12,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`connected to ${PORT}`));
